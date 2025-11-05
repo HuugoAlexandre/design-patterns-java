5bc496ebcdacf0bc736aad99e244ea19a00ade4d
HuugoAlexandre <huugoalexanndre@gmail.com>
2025-11-05 14:49:00 -0300

    Model

---
+++ 1.2/src/main/java/com/example/demo/model/NotificationRequest.java
@@ -0,0 +1,36 @@
+package com.example.demo.model;
+
+public class NotificationRequest {
+    private String type;
+    private String recipient;
+    private String subject;
+    private String content;
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
